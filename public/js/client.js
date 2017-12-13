19eae2c69fd089cf9e1f08bb24cb749e6b2dc8d9
Glitch (tasteful-sagittarius) <none>
2017-12-13 00:28:54 +0000

    🚇📌 Updated with Glitch

--- public/js/client.js
+++ public/js/client.js
@@ -1,8 +1,8 @@
 /* global TrelloPowerUp */
 
-var Promise = TrelloPowerUp.Promise;
+//var Promise = TrelloPowerUp.Promise;
 
-var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
+//var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
 
 TrelloPowerUp.initialize({
   // Start adding handlers for your capabilities here!
